c1caca3580d7c05906aa29fdcb0204a727f7f3fa
Alicia-09 <24308060610613@cetis61.edu.mx>
2025-09-25 08:28:16 -0600

    calculadora

--- app.py
+++ app.py
@@ -3,12 +3,65 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hola_mundo():
-    return "<h1> ¡Hola Alicia!</h1>"
+def home():
+    mensaje = "<h1> ¡Bienvenido a la calculadora de Flask!</h1>"
+    
+    mensaje += "<h2> Para poder usarla tendras que hacer</h2>"
+    mensaje += "<ol>"
+    mensaje += "<li><h2>Sumar http://127.0.0.1:5000//sumar/10/20</h2><li>"
+    mensaje += "<li><h2>Restar http://127.0.0.1:500/restar/10/20</h2><li>"
+    mensaje += "<li><h2>Multiplicar http://127.0.0.1:500/multiplicar/10/20</h2><li>"
+    mensaje += "<li><h2>Dividir http://127.0.0.1:500/dividir/10/20</h2><li>"
+    mensaje += "<li><h2>Maximo http://127.0.0.1:500/maximo/10/20</h2><li>"
+    mensaje += "<li><h2>Minimo http://127.0.0.1:500/minimo/10/20</h2><li>"
+    mensaje += "</ol>"
+    return mensaje
 
-@app.route("/nombre")
-def hola_mundo2():
-    return "<h1> ¡Hola Camacho y Alicia!</h1>"
+@app.route("/sumar/<v1>/<v2>")
+def sumar1(v1,v2):
+    s=str(float(v1)+ float (v2))
+    mensaje= f"<h1>La suma de {v1} + {v2} es {s}</h1>"
+    return mensaje
+
+@app.route("/restar/<v1>/<v2>")
+def restar1(v1,v2):
+    r=str(float(v1) - float (v2))
+    mensaje= f"<h1>La resta de {v1} - {v2} es {r}</h1>"
+    return mensaje
+
+@app.route("/multiplicar/<v1>/<v2>")
+def multiplicar1(v1,v2):
+    m=str(float(v1) *  float (v2))
+    mensaje= f"<h1>La multiplicacion de {v1} * {v2} es {m}</h1>"
+    return mensaje
+
+@app.route("/dividir/<v1>/<v2>")
+def dividir1(v1,v2):
+    d=str(float(v1) / float (v2))
+    mensaje= f"<h1>La division de {v1} / {v2} es {d}</h1>"
+    return mensaje
+
+@app.route("/maximo/<v1>/<v2>")
+def maximo1(v1,v2):
+    n1 = float(v1)
+    n2 = float(v2)
+    if n1 > n2:
+        mayor = n1
+    else:
+        mayor = n2
+    mensaje= f"<h1>El maximo entre {n1} y  {n2} es {mayor}</h1>"
+    return mensaje
+
+@app.route("/Minimo/<v1>/<v2>")
+def minimo1(v1,v2):
+    n1 = float(v1)
+    n2 = float(v2)
+    if n1 < n2:
+        menor = n1
+    else:
+        menor = n2
+    mensaje= f"<h1>El minimo entre {n1} y  {n2} es {menor}</h1>"
+    return mensaje
 
 if __name__ == "__main__":
     app.run(debug=True)
